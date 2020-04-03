dfc8b4bcbb25793a8d6465245987e8796e674e9d
KubaBury <bureja11@seznam.cz>
2020-04-03 13:56:16 +0200

    updated scripts and thesis

---
+++ scripts/elbo_polynom.jl
@@ -0,0 +1,33 @@
+using Flux
+using Plots
+using LaTeXStrings
+using SpecialFunctions
+using LinearAlgebra
+using Distributions
+
+# define LS
+
+x = 1:2;
+y = x.^2 .+ 1 .+ 1.0*randn(size(x));
+X=[ones(size(x)) x.^2 x.^3 x.^4 x.^5];
+
+#initial values of optimization
+θh = randn(5,1);
+A = randn((5,5));
+γ = [2.0];
+δ = [1.0];
+
+# define loss function
+Entq(Σ) = -0.5*log(det(Σ))
+Entq(γ,δ) = -γ.-log.(δ.*gamma.(γ)).+(1 .+γ).*digamma.(γ)
+Elogp(X,y,θh,Σ,γ,δ) = 0.5*(y'*y.-θh'*X'*y.-y'*X*θh+
+tr((X'*X+δ./(γ.-1).*I).*(θh*θh'+Σ))+log.(δ).-digamma.(γ))
+loss(X,y,θh,Σ,γ,δ)= Entq(Σ)+Entq(γ,δ)+Elogp(X,y,θh,Σ,γ,δ)
+
+
+data = Iterators.repeated((X,y),10000)
+Flux.train!((X,y)->loss(X,y,θh,A*A',γ,δ),[θh,A,γ,δ],data,ADAM(0.01))
+   
+
+
+
---
+++ scripts/myelbo.jl
@@ -0,0 +1,51 @@
+using Flux
+using Plots
+using LaTeXStrings
+using SpecialFunctions
+
+py1(y1,θ,s) = 1/sqrt(s)*exp(-0.5*(y1-θ)^2/s);
+py2(y2,θ,s) = 1/sqrt(s)*exp(-0.5*(y2-θ)^2/s);
+pθ(θ,α) = 1/sqrt(α)*exp(-0.5*θ^2/(α));
+pα(α) = α^(-1); #Jeffrey
+
+
+joint(y1,y2,θ,α,s) = py1(y1,θ,s)*py2(y2,θ,s)*pθ(θ,α)*pα(α);
+
+plc=contour(9.5:0.01:13,8:0.01:400,(θ,α)->joint(11.,12.,θ,α,1.), xlabel=L"$\theta$", ylabel=L"$\alpha$", levels=6)
+
+# elbo
+#qθ = N(μ,σ) =>
+#qα = iG(γ,δ) =>
+
+using Distributions
+Entq_θ(σ) = -0.5*log.(σ)
+Entq_α(γ,δ) = -γ.-log.(δ.*gamma.(γ)).+(1 .+ γ).*digamma.(γ)
+Elogp_y1(y1,μ,σ) = -0.5*(y1.^2 -2*y1.*μ.+μ.^2 .+σ)
+Elogp_y2(y2,μ,σ) = -0.5*(y2^.2 -2*y2.*μ.+μ.^2 .+σ)
+Elogp_θ(μ,σ,γ,δ) = -0.5*(μ.^2 .+ σ).*γ./δ
+Elogp_α(γ,δ) = digamma.(γ).-log.(δ)
+
+
+
+elbo2(y1,y2,μ,σ,γ,δ) = sum(Elogp_y1(y1,μ,σ) + Elogp_y2(y2,μ,σ) +
+ Elogp_θ(μ,σ,γ,δ) + Elogp_α(γ,δ) - Entq_θ(σ) - Entq_α(γ,δ))
+
+μ = [10.0];
+σ = [100.0];
+γ = [1.0];
+δ = [1.0];
+
+using Distributions
+loss_elbo(y1,y2) = -elbo2(y1,y2,μ,σ,γ,δ)
+mydata = Iterators.repeated(([11.0;12.0],), 1000)
+TH=zeros(4,13);
+for epo=1:13
+    global plc
+    global TH
+    Flux.train!(loss_elbo,Flux.params([μ,σ,γ,δ]),mydata,ADAM(0.01))
+    plt(θ,α) = pdf(Normal(μ[1],sqrt(σ[1])),θ) .* pdf.(InverseGamma(γ[1],δ[1]), α)
+    contour!(plc,10:0.01:14,0:0.1:1.5,plt,levels=5, linestyle=:dash)
+    TH[:,epo].=vcat(μ,σ,γ,δ);
+end
+TH
+gui()
