bea70c62e4af91d32906dc6094279f6bfac8b5ff
KubaBury <bureja11@seznam.cz>
2020-07-11 11:08:39 +0200

    VAE_example

--- scripts/my_vae.jl
+++ scripts/my_vae.jl
@@ -2,6 +2,7 @@ using Plots;
 ## Generate data
 using Statistics
 using Distributions
+using Flux
 ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## 
 i = 200 ;#celkovy pocet y_i;
 λ = 10 ;#rate in Poisson distribution;
@@ -10,15 +11,15 @@ U = Uniform(0,10);
 pocet_x = rand(P,i); #pocty x_k prirazene k jednomu y_i
 xx = [rand(U, pocet_x[j]) for j =1:i] ##  pole poli, generuje bag
 x = mean.(xx); ## prumery jednotlivych instanci
-y = 1 .+ mx .+ mx.^2 .+ rand(length(mx)); ## kvadraticka zavislost + noise
+y = 1 .+ x .+ x.^2 .+ rand(length(x)); ## kvadraticka zavislost + noise
 ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## ## 
 
 ## normalize data!!!
 p_hx=Plots.histogram(x',nbins=40);#,xlabel='x',ylabel='count');
 
 ## NN == inicializace??
-nx = 2
-nz = 2
+nx = 2;
+nz = 2;
 nh = 2;
 #A, μ, logσ = Dense(nx, nh, swish), Dense(nh, nz), Dense(nh, nz)
 A, μ, logσ = Dense(nx, nh), Dense(nh, nz), Dense(nh, nz)
@@ -50,3 +51,12 @@ for n=1:10
     Flux.train!(loss,ps,da,opt);
     println(loss(x))
 end
+
+xg = -3:0.1:7; # grid of x
+zg = -2:0.1:2; # grid of z
+
+
+z_xg = Flux.data(μ(A(xg')));
+x_zg = Flux.data(f(zg'));
+plot(xg,z_xg[:];label="ecoder");
+plot!(x_zg[:],zg;label="decoder")
